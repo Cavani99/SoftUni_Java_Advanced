1df87cd506b02f0031c1c0669a12387df39ffc52
Cavani99 <kristiian.donchev@abv.bg>
2024-09-12 20:42:47 +0300

    Fixed task 7 Stacks and queues exercises

--- Stacks and Queues Exercises/src/Simple_Text_Editor.java
+++ Stacks and Queues Exercises/src/Simple_Text_Editor.java
@@ -7,65 +7,29 @@ public class Simple_Text_Editor {
         Scanner scanner = new Scanner(System.in);
         ArrayDeque<String> queue = new ArrayDeque<>();
         int commands = Integer.parseInt(scanner.nextLine());
-        ArrayDeque<String> prev =  new ArrayDeque<>();
-        ArrayDeque<String> copy =  new ArrayDeque<>();
+        StringBuilder text = new StringBuilder();
 
         for (int i = 0; i < commands; i++) {
             String [] command = scanner.nextLine().split("\\s+");
 
             if(command[0].equals("1")){
 
-                //retrieve information before operation
-                if(queue.isEmpty()){
-                    prev.offer("");
-                } else {
-                    StringBuilder queueInfo = new StringBuilder();
-                    copy = queue.clone();
-                    while (!queue.isEmpty()){
-                        queueInfo.append(queue.poll());
-                    }
-                    queue = copy.clone();
-                    prev.offer(String.valueOf(queueInfo));
-                }
-
-                //operation
-                String text = command[1];
-                for (int j = 0; j < text.length(); j++) {
-                  queue.offer(String.valueOf(text.charAt(j)));
-                }
+                queue.offer(text.toString());
+                text.append(command[1]);
             } else if(command[0].equals("2")){
-                StringBuilder text = new StringBuilder();
-                copy = queue.clone();
-               while (!queue.isEmpty()){
-                    text.append(queue.poll());
-                }
-                queue = copy.clone();
-                for (int j = 0; j < Integer.parseInt(command[1]); j++) {
-                    if(!queue.isEmpty()){
-                        queue.pollLast();
-                    }
-                }
-                prev.offer(text.toString());
+                queue.offer(text.toString());
+
+                int characters = Integer.parseInt(command[1]);
+                text.delete(text.length()-characters, text.length());
+
             } else if(command[0].equals("3")){
-                ArrayDeque<String> copyQueue = queue.clone();
-
-                for (int j = 0; j < Integer.parseInt(command[1]) - 1; j++) {
-                    if(!queue.isEmpty())
-                        queue.pollFirst();
-                }
-                System.out.println(queue.pollFirst());
-                queue = copyQueue.clone();
+                int position = Integer.parseInt(command[1]) - 1;
+                System.out.println(text.charAt(position));
             } else if(command[0].equals("4")){
-                String text = prev.poll();
-                queue.clear();
-
-                for (int j = 0; j < text.length(); j++) {
-                    queue.offer(String.valueOf(text.charAt(j)));
-                }
+                text = new StringBuilder(queue.pollLast());
 
             }
         }
-
 
     }
 }
