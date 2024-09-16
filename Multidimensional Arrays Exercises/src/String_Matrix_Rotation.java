d6cc81d4b491e7562e14cc285429c1303762f700
Cavani99 <kristiian.donchev@abv.bg>
2024-09-16 21:02:09 +0300

    Finished tasks 6 Multidimensional arrays exercises

--- Multidimensional Arrays Exercises/src/String_Matrix_Rotation.java
+++ Multidimensional Arrays Exercises/src/String_Matrix_Rotation.java
@@ -4,7 +4,48 @@ import java.util.Scanner;
 
 public class String_Matrix_Rotation {
 
-    public static void main(String[] args) {
+        public static void printMatrix(String [] [] matrix){
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if(matrix[i][j].equals("whitespace"))
+                        System.out.print(" ");
+                    else
+                        System.out.print(matrix[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+    static String[][] rotate90(String[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // Determine the dimensions of the rotated matrix
+        int newRows = cols;
+        int newCols = rows;
+
+        // Create a new matrix for the rotated result
+        String[][] rotatedMatrix = new String[newRows][newCols];
+
+        // Initialize the rotated matrix with empty strings
+        for (int i = 0; i < newRows; i++) {
+            for (int j = 0; j < newCols; j++) {
+                rotatedMatrix[i][j] = "";
+            }
+        }
+
+        // Fill the rotated matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                rotatedMatrix[j][rows - 1 - i] = matrix[i][j];
+            }
+        }
+
+        return rotatedMatrix;
+    }
+
+
+        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String [] rotation = scanner.nextLine().split("\\D+");
         int degrees = Integer.parseInt(rotation[1]);
@@ -19,17 +60,29 @@ public class String_Matrix_Rotation {
             elements++;
             if(command.length() > longestText)
                 longestText = command.length();
+
+
             command = scanner.nextLine();
         }
 
-        String [] [] matrix;
-        if(degrees % 180 == 0){
-            matrix = new String[elements][longestText];
-        } else {
-            matrix = new String[longestText][elements];
+        String[][] matrix = new String[elements][longestText];
+        for (int i = 0; i < matrix.length; i++) {
+            Arrays.fill(matrix[i], "whitespace");
         }
 
-        System.out.println(Arrays.deepToString(matrix));
+        for (int i = 0; i < strings.size(); i++) {
+            String current = strings.get(i);
+            for (int j = 0; j < current.length(); j++) {
+                matrix[i][j] = String.valueOf(current.charAt(j));
+            }
+        }
+        int rotations = degrees / 90 ;
 
+        String[][] rotatedMatrix = matrix; // Start with the original matrix
+        for (int i = 0; i < rotations; i++) {
+            rotatedMatrix = rotate90(rotatedMatrix);
+        }
+
+        printMatrix(rotatedMatrix);
     }
 }
