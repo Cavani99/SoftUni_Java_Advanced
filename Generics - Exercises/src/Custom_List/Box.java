3adeb40105c04b3a03fd927c93967d0b51e053f6
Cavani99 <kristiian.donchev@abv.bg>
2024-10-07 20:33:58 +0300

    Made tasks 7-8 in Generics - Exercises

---
+++ Generics - Exercises/src/Custom_List/Box.java
@@ -0,0 +1,76 @@
+package Custom_List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Comparable<T>> {
+
+    private final ArrayList<T> list;
+
+    Box() {
+        this.list = new ArrayList<>();
+    }
+
+    public int greater(T element) {
+        int count = 0;
+
+        for (int i = 0; i < this.list.size(); i++) {
+            if(this.list.get(i).compareTo(element) > 0)
+                count++;
+        }
+
+        return count;
+    }
+
+    public void add(T value){
+        this.list.add(value);
+    }
+
+    public T remove(int index){
+        return this.list.remove(index);
+    }
+
+    public boolean contains(T element){
+        return this.list.contains(element);
+    }
+
+    public void swap(int n1, int n2){
+        if(n1 < this.list.size() && n2 < this.list.size()) {
+            T value = this.list.get(n1);
+
+            this.list.set(n1, this.list.get(n2));
+            this.list.set(n2, value);
+        }
+    }
+
+    public T getMax(){
+        if(this.list.isEmpty())
+            throw new IllegalArgumentException();
+        T max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (max.compareTo(list.get(i)) < 0)
+                max = list.get(i);
+        }
+        return max;
+    }
+
+    public T getMin(){
+        if(list.isEmpty())
+            throw new IllegalArgumentException();
+        T min = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (min.compareTo(list.get(i)) > 0)
+                min = list.get(i);
+        }
+        return min;
+    }
+
+    public void printList() {
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println(String.format("%s", this.list.get(i)));
+        }
+
+    }
+
+
+}
---
+++ Generics - Exercises/src/Custom_List_Sorter/Main.java
@@ -0,0 +1,46 @@
+package Custom_List_Sorter;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box<String> box = new Box<>();
+        String [] command = scanner.nextLine().split("\\s+");
+        while (!command[0].equals("END")){
+            switch (command[0]){
+                case "Add" :
+                    box.add(command[1]);
+                    break;
+                case "Remove":
+                    box.remove(Integer.parseInt(command[1]));
+                    break;
+                case "Contains":
+                    System.out.println(box.contains(command[1]));
+                    break;
+                case "Greater":
+                    System.out.println(box.greater(command[1]));
+                    break;
+                case "Swap":
+                    box.swap(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+                    break;
+                case "Max":
+                    System.out.println(box.getMax());
+                    break;
+                case "Min":
+                    System.out.println(box.getMin());
+                    break;
+                case "Print":
+                    box.printList();
+                    break;
+                case "Sort":
+                    box.sort();
+                    break;
+            }
+
+            command = scanner.nextLine().split("\\s+");
+        }
+    }
+}
